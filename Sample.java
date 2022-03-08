575361d92dd41a78747e25071ed5a0ed9b6b71fb
pradyumna1226 <97030417+pradyumna1226@users.noreply.github.com>
2022-03-08 18:01:19 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,14 @@
+class Sample{
+      public static void main(String[] args){
+           
+           int a=10;
+           int b=20;
+           int c=30;
+           int d=a++ + b++ + c++ + a++ + b++;
+           System.out.println(a);
+           System.out.println(b);
+           System.out.println(c);
+           System.out.println(d);
+
+   }
+}
